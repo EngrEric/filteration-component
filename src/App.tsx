a83aa267fe3ff18c20213c00234819b1aac46607
engr.eric <okemmadueric@gmail.com>
2024-01-16 15:06:49 +0100

    feat: app bootstraping

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import CardList from "./components/core/CardList";
+import { filterByType } from "./utils/helpers";
+import {
+  FilterType,
+  ResponseDataObject,
+  sampleResponseData
+} from "./components/constants";
+import { FilterContext } from "./RootContext";
 
 function App() {
+  const responseData = sampleResponseData.data.oneClickAutomations.items;
+  const [data, setData] = useState<ResponseDataObject[]>(responseData);
+  const [removeAllFilter, setRemoveAllFilter] = useState<boolean>(false);
+
+  const handleFilter = useCallback(
+    (filters: Array<string>, filterTypes: FilterType) => {
+      const filteredData = filterByType(filters, responseData, filterTypes);
+
+      setData(filteredData);
+      setRemoveAllFilter(filterTypes === FilterType.all);
+    },
+    [responseData]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FilterContext.Provider value={{ data, handleFilter, removeAllFilter }}>
+      <div className="md:max-w-[70vw] md:mx-auto mx-6">
+        <div className="flex my-10 items-center justify-between">
+          <p className="text-gray-500">
+            Here are some Automations that pre-defined for product availability
+            monitoring:
+          </p>
+          <button
+            onClick={() => handleFilter([], FilterType.all)}
+            className="btn btn-link"
+          >
+            See all
+          </button>
+        </div>
+        <CardList />
+      </div>
+    </FilterContext.Provider>
   );
 }
 
 export default App;
+
